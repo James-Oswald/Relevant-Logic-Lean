4297a72bf1171328bd8718a5dbce65faccea1ab1
James Oswald <jamesoswald111@gmail.com>
2024-09-20 11:18:19 -0400

    new approach

--- RLL/B.lean
+++ RLL/B.lean
@@ -1,4 +1,7 @@
 
+
+import Mathlib.Tactic.Contrapose
+
 inductive Formula : Type where
 | Atom : Nat -> Formula
 | Not : Formula -> Formula
@@ -25,24 +28,60 @@ structure RMFrame where
   C4 (a : W) : S (S a) = a
   C5 (a b : W) : R O a b -> R O (S b) (S a)
 
+def Leqr {F : RMFrame} (a b : F.W) : Prop := F.R F.O a b
+notation a "≤ᵣ" b => Leqr a b
+
 -- A Routley–Meyer model is a frame along with a valuation function
+-- That satisfies certain conditions
 structure RMModel where
   F : RMFrame
-  V : Nat → F.W -> Prop -- We use a relation instead : V(atom, w) == w ∈ v(atom)
+  -- Valuation function on atoms
+  VA : F.W -> Nat -> Prop -- We use a relation instead : V(w, atom) == w ∈ v(atom)
+  V : F.W -> Formula -> Prop
+  -- Conditions on the valuation function for compound formulas
+  V_atom (n : Nat) (w : F.W) : V w (Formula.Atom n) = VA w n
+  V_not (φ : Formula) (w : F.W) : V w (¬ᵣφ) = ¬V (F.S w) φ
+  V_and (φ ψ : Formula) (w : F.W) : V w (φ ∧ᵣ ψ) = V w φ ∧ V w ψ
+  V_or (φ ψ : Formula) (w : F.W) : V w (φ ∨ᵣ ψ) = V w φ ∨ V w ψ
+  V_imp (φ ψ : Formula) (a : F.W) : V w (φ →ᵣ ψ) = ∀ (b c : F.W), F.R a b c → V b φ → V c ψ
+  --Hereditariness condition on atoms
+  V_hered (n : Nat) (a b : F.W) : V a (Formula.Atom n) -> (a ≤ᵣ b) -> V b (Formula.Atom n)
 
-
-def entails (M : RMModel) (a : M.F.W) : Formula -> Prop
-| Formula.Atom n => M.V n a
-| Formula.Not φ => ¬entails M (M.F.S a) φ
-| Formula.And φ ψ => entails M a φ ∧ entails M a ψ
-| Formula.Or φ ψ => entails M a φ ∨ entails M a ψ
-| Formula.Imp φ ψ => ∀ (b c : M.F.W), M.F.R a b c → entails M b φ → entails M c ψ
-
-notation M ":" w "⊨" φ => entails M w φ
+-- Need this helper def for the notation
+def valuation (M : RMModel) (w : M.F.W) (φ : Formula) : Prop := M.V w φ
+notation M ":" w "⊨" φ => valuation M w φ
 
 def valid (φ : Formula) : Prop := ∀ (M : RMModel) (w : M.F.W), M : w ⊨ φ
-
 notation "⊨" φ => valid φ
+
+-- The hereditariness condition extends from atoms to all formulas
+theorem hered_all (M : RMModel) (a b : M.F.W) (φ : Formula):
+(M : a ⊨ φ) -> (a ≤ᵣ b) -> M : b ⊨ φ := by
+intros H1 H2
+induction φ
+. case Atom n =>
+  exact M.V_hered n a b H1 H2
+. case Not φ F =>
+  simp [valuation] at *;
+  rw [M.V_not φ a] at H1;
+  rw [M.V_not φ b]
+  contrapose! H1
+  sorry
+
+
+
+
+
+-- Hopefully we can prove hereditariness condition as traditionally presented from our
+-- addition of it on entails.
+
+
+
+
+
+
+
+
 
 
 theorem ax1_valid : ⊨ (A →ᵣ A) := by
