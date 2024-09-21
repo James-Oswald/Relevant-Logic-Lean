3c96b6a06ae5ab78381eeba0724de19a1f52db00
James Oswald <jamesoswald111@gmail.com>
2024-09-21 15:13:58 -0400

    pain peko

--- RLL/B.lean
+++ RLL/B.lean
@@ -1,6 +1,7 @@
 
-
+import Aesop
 import Mathlib.Tactic.Contrapose
+
 
 inductive Formula : Type where
 | Atom : Nat -> Formula
@@ -12,10 +13,10 @@ inductive Formula : Type where
 instance : Coe Nat Formula := ⟨Formula.Atom⟩
 
 --Notations for term level formulae
-prefix:80 "¬ᵣ" => Formula.Not
-infixr:70 "∧ᵣ" => Formula.And
-infixr:65 "∨ᵣ" => Formula.Or
-infixr:60 "→ᵣ" => Formula.Imp
+prefix:80 " ¬ᵣ " => Formula.Not
+infixr:70 " ∧ᵣ " => Formula.And
+infixr:65 " ∨ᵣ " => Formula.Or
+infixr:60 " →ᵣ " => Formula.Imp
 
 -- Unconditioned Routley–Meyer frames
 structure URMFrame where
@@ -25,18 +26,18 @@ structure URMFrame where
   O : W         -- 0
 
 def URMFrame.Star {F : URMFrame} (a : F.W) : F.W := F.S a
-prefix:80 "*ᵣ"  => URMFrame.Star
+postfix:80 "*ᵣ"  => URMFrame.Star
 
 def URMFrame.Leq {F : URMFrame} (a b : F.W) : Prop := F.R F.O a b
-infix:70 "≤ᵣ" => URMFrame.Leq
+infix:70 " ≤ᵣ " => URMFrame.Leq
 
 -- Conditioned Routley–Meyer frames
 structure RMFrame extends URMFrame where
-  C1 (a : W) : a ≤ᵣ a
-  C2 (a b c : W) : a ≤ᵣ b → b ≤ᵣ c → a ≤ᵣ c
-  C3 (a b c d : W) : d ≤ᵣ a -> R a b c -> R d b c
-  C4 (a : W) : *ᵣ*ᵣa = a
-  C5 (a b : W) : a ≤ᵣ b -> *ᵣa ≤ᵣ *ᵣb
+  leq_symm  (a : W)       : a ≤ᵣ a
+  leq_trans (a b c : W)   : a ≤ᵣ b → b ≤ᵣ c → a ≤ᵣ c
+  leq_?     (a b c d : W) : d ≤ᵣ a -> R a b c -> R d b c
+  star_star (a : W)       : a*ᵣ*ᵣ = a
+  leq_star  (a b : W)     : a ≤ᵣ b -> b*ᵣ ≤ᵣ a*ᵣ
 
 -- An Unconditioned Routley–Meyer model is a frame along with a valuation function
 structure URMModel extends RMFrame where
@@ -45,23 +46,75 @@ structure URMModel extends RMFrame where
 
 -- Notation for a formula being true at a world in a model
 def URMModel.holds {M : URMModel} (w : M.W) (φ : Formula) : Prop := M.V w φ
-infix:50 "⊩" => URMModel.holds
+infix:50 " ⊩ " => URMModel.holds
 def URMModel.nholds {M : URMModel} (w : M.W) (φ : Formula) : Prop := ¬(w ⊩ φ)
-infix:50 "⊮" => URMModel.nholds
+infix:50 " ⊮ " => URMModel.nholds
 
 -- Conditioned Routley–Meyer models
 structure RMModel extends URMModel where
   -- Basic Conditions
-  V_not (φ : Formula)   (w : W) :    (w ⊩ ¬ᵣφ) ↔ (*ᵣw ⊮ φ)
+  V_not (φ : Formula)   (w : W) :    (w ⊩ ¬ᵣφ) ↔ (w*ᵣ ⊮ φ)
   V_and (φ ψ : Formula) (w : W) : (w ⊩ φ ∧ᵣ ψ) ↔ (w ⊩ φ) ∧ (w ⊩ ψ)
   V_or  (φ ψ : Formula) (w : W) : (w ⊩ φ ∨ᵣ ψ) ↔ (w ⊩ φ) ∨ (w ⊩ ψ)
-  V_imp (φ ψ : Formula) (a : W) : (w ⊩ φ →ᵣ ψ) ↔ ∀ b c, R a b c → b ⊩ φ → c ⊩ ψ
+  V_imp (φ ψ : Formula) (a : W) : (w ⊩ φ →ᵣ ψ) ↔ ∀ b c, (R a b c ∧ b ⊩ φ) → (c ⊩ ψ)
   -- Hereditariness on atoms condition
-  V_her (n : Nat) (a b : W) : a ⊩ n → a ≤ᵣ b → b ⊩ n
+  V_her (n : Nat) (a b : W) : (a ⊩ n) → (a ≤ᵣ b) → (b ⊩ n)
 
 def valid (φ : Formula) : Prop := ∀ (M : RMModel) (w : M.W), w ⊩ φ
-notation "⊨" φ => valid φ
+notation "⊨ " φ => valid φ
 
+lemma RMModel.V_not' (M : RMModel) (φ : Formula) (w : M.W) : (w*ᵣ ⊩ ¬ᵣφ) ↔ (w ⊮ φ) := by
+  apply Iff.intro
+  . case mp =>
+    intro H
+    simp [M.V_not, M.star_star] at H
+    exact H
+  . case mpr =>
+    intro H
+    simp [M.V_not, M.star_star]
+    exact H
+
+lemma RMModel.V_not'' (M : RMModel) (φ : Formula) (w : M.W) : (w*ᵣ ⊮ ¬ᵣφ) ↔ (w ⊩ φ) := by
+  apply Iff.intro
+  . case mp =>
+    intro H
+    simp [URMModel.nholds, M.V_not, M.star_star] at H
+    exact H
+  . case mpr =>
+    intro H
+    simp [URMModel.nholds, M.V_not, M.star_star]
+    exact H
+
+lemma RMModel.V_not''' (M : RMModel) (φ : Formula) (w : M.W) : (w ⊮ ¬ᵣφ) ↔ (w*ᵣ ⊩ φ) := by
+  apply Iff.intro
+  . case mp =>
+    intro H
+    simp [URMModel.nholds, M.V_not, M.star_star] at H
+    exact H
+  . case mpr =>
+    intro H
+    simp [URMModel.nholds, M.V_not, M.star_star]
+    exact H
+
+lemma starm (M : RMModel) (φ : Formula) (w : M.W) : (w*ᵣ ⊮ φ) ↔ (w ⊩ φ) := by
+  apply Iff.intro
+  . case mp =>
+    sorry
+  . case mpr =>
+    intro H
+    by_contra H
+    simp [URMModel.nholds, M.V_not, M.star_star] at H
+
+
+--This is totally in Mathlib somewhere but I sure can't find it
+lemma impl_or_n {a b : Prop} : (a → b) ↔ (¬a ∨ b) := by
+  apply Iff.intro
+  . case mp =>
+    contrapose!
+    simp only [imp_self]
+  . case mpr =>
+    contrapose!
+    simp only [imp_self]
 
 -- The hereditariness condition extends from atoms to all formulas
 theorem hered_all (M : RMModel) (a b : M.W) (φ : Formula):
@@ -70,19 +123,49 @@ theorem hered_all (M : RMModel) (a b : M.W) (φ : Formula):
   induction φ
   . case Atom n =>
     exact M.V_her n a b H1 H2
-  . case Not φ F =>
-    simp [valuation] at *;
-    rw [M.V_not φ a] at H1;
-    rw [M.V_not φ b]
-    contrapose! H1
+  . case Not φ ih =>
+    simp [M.V_not] at *
     sorry
+    -- induction φ
+    -- . case Atom n =>
+    --   simp_all only [M.V_not]
+    --   have r : b*ᵣ ≤ᵣ a*ᵣ := M.leq_star a b H2
+    --   contrapose! H1
+    --   simp_all only [URMModel.nholds, not_not]
+    --   exact M.V_her n (b*ᵣ) (a*ᵣ) H1 r
+    -- . case Not φ ih' =>
+    --   contrapose! ih'
+    --   simp [M.V_not, URMModel.nholds, M.star_dneg] at H1 ih'
+    --   sorry
+    -- . case And φ ψ ih1' ih2' =>
+    --   simp_all only [M.V_not, M.V_and, URMModel.nholds, not_and_or]
+    --   cases H1
+    --   . case inl C =>
+    --     apply Or.inl
+    --     sorry
+    --   . case inr C =>
+    --     apply Or.inr
+    --     sorry
+    -- . case Or φ ψ ih1' ih2' =>
+    --   simp_all only [M.V_not, M.V_or, URMModel.nholds, not_or]
+    --   apply And.intro
+    --   . case left =>
+    --     apply ih1'
+    --     sorry
+    --   . case right =>
+    --     sorry
+    -- . case Imp φ ψ ih1' ih2' =>
+    --   simp_all only [M.V_not, URMModel.nholds]
+    --   rw [M.V_imp] at *
+    --   sorry
+    --   sorry
   . case And φ ψ ih1 ih2 =>
-    simp [valuation, M.V_and] at *;
+    simp [M.V_and] at *;
     apply And.intro
     . exact ih1 H1.left
     . exact ih2 H1.right
   . case Or φ ψ ih1 ih2 =>
-    simp [valuation, M.V_or] at *;
+    simp [M.V_or] at *;
     cases H1
     . case inl C =>
       apply Or.inl
@@ -91,30 +174,15 @@ theorem hered_all (M : RMModel) (a b : M.W) (φ : Formula):
       apply Or.inr
       exact ih2 C
   . case Imp φ ψ ih1 ih2 =>
-    simp [valuation, M.V_imp] at *;
-    intros b c R1 R2
-    apply ih2
-    . exact H1 (b := b) R1
-    . exact H2 (b := c) R2
-
-
-
-
-
--- Hopefully we can prove hereditariness condition as traditionally presented from our
--- addition of it on entails.
-
-
-
-
-
-
-
-
+    simp only [M.V_imp φ ψ M.O] at *
+    intros b' c'
+    exact H1 b' c'
 
 
 theorem ax1_valid : ⊨ (A →ᵣ A) := by
-  sorry
+  intros M w
+  simp [M.V_imp A A M.O]
+  intros b c
 
 
 -- Adding Raaa as a frame condition allows us to validate the psudo-modus ponens axiom
